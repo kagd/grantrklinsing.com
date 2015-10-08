cd09c5285c3cfd449a3c6cc4e40d7eb87ac673f9
Grant Klinsing <grant@digitalopera.com>
2015-10-07 23:07:47 -0600

    update config

--- config.rb
+++ config.rb
@@ -35,10 +35,9 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  deploy.branch   = 'master' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  deploy.remote   = 'git@github.com:kagd/kagd.github.io.git'
+  deploy.branch   = 'master'
+  deploy.strategy = :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
