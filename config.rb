ca06d00f1197579760fd5e5e4e368cb104c99d91
Grant Klinsing <grant@digitalopera.com>
2016-05-26 21:06:08 -0600

    update node_modules

--- config.rb
+++ config.rb
@@ -47,7 +47,7 @@ page "/feed.xml", layout: false
 sprockets.append_path File.join root, 'node_modules', 'foundation-apps', 'dist', 'css'
 sprockets.append_path File.join root, 'node_modules', 'foundation-apps', 'dist', 'js'
 sprockets.append_path File.join root, 'node_modules', 'perfect-scrollbar', 'dist', 'css'
-sprockets.append_path File.join root, 'node_modules', 'perfect-scrollbar', 'dist', 'js', 'min'
+sprockets.append_path File.join root, 'node_modules', 'perfect-scrollbar', 'dist', 'js'
 sprockets.append_path File.join root, 'node_modules', 'angular-perfect-scrollbar', 'src'
 
 ###
