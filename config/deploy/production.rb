ea7b9b22e40ed5c5cebff1494031952acbcfd36e
Grant Klinsing <grant@digitalopera.com>
2017-08-17 21:20:15 -0500

    update css for necro

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :application, 'client_site'
 # set :state,       :production
 
 ## Servers we are going to deploy to ----------------------
-server '159.203.78.230', user: 'deploy'
+server '138.197.1.41', user: 'deploy'
 
 ## Server Settings ----------------------------------------
 set :deploy_to, "/var/www/client_site"
